146d7076512da20f97debd58b65de53582cf1a6f
guilherme <guilhermefrrbtt1@hotmail.com>
2021-06-24 01:24:30 -0300

    Working on CRUD operations

--- maven-jdbc/src/main/java/ghost/ApplicationMain.java
+++ maven-jdbc/src/main/java/ghost/ApplicationMain.java
@@ -8,8 +8,18 @@ public class ApplicationMain {
         CadastroDAO dao = new CadastroDAO();
 
         Cadastro cadastro = new Cadastro();
-        cadastro.setNome("Bruce Wayne");
-        cadastro.setTelefone(11977042333L);
+        cadastro.setNome("Jason Todd");
+        cadastro.setTelefone(1199998777L);
         dao.incluir(cadastro);
+
+//        Cadastro cadastro2 = new Cadastro();
+//        cadastro2.setNome("TESTE");
+//        cadastro2.setTelefone(888123781L);
+//        cadastro2.setId(3);
+//        dao.alterar(cadastro2);
+
+        //dao.delete(3);
+        dao.listar();
+
     }
 }
--- maven-jdbc/src/main/java/ghost/dao/CadastroDAO.java
+++ maven-jdbc/src/main/java/ghost/dao/CadastroDAO.java
@@ -3,10 +3,9 @@ package ghost.dao;
 import ghost.connection.jdbc.ConnectionManager;
 import ghost.model.Cadastro;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CadastroDAO {
     private Connection connection;
@@ -20,18 +19,62 @@ public class CadastroDAO {
         //connection.close();
         try {
             String sql = "INSERT INTO public.tb_mjv (nome, telefone) VALUES (?, ?);";
-            PreparedStatement insert = connection.prepareStatement(sql);
-            insert.setString(1, cadastro.getNome());
-            insert.setLong(2, cadastro.getTelefone());
-            insert.execute();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, cadastro.getNome());
+            statement.setLong(2, cadastro.getTelefone());
+            statement.execute();
         } catch (SQLException e){
             e.printStackTrace();
         }
 
     }
 
-    public void alterar(){
-        //connection.close();
+    public void alterar(Cadastro cadastro){
+        try {
+            String sql = "UPDATE public.tb_mjv SET nome=?, telefone=? WHERE id=?;";
+            PreparedStatement statement =  connection.prepareStatement(sql);
+            statement.setString(1, cadastro.getNome());
+            statement.setLong(2, cadastro.getTelefone());
+            statement.setInt(3, cadastro.getId());
+            statement.execute();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
+    public List<Cadastro> listar(){
+        List<Cadastro> cadastros = new ArrayList<Cadastro>();
+        try{
+            String sql = "SELECT * FROM public.tb_mjv;";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()){
+                Cadastro cadastro = new Cadastro();
+                cadastro.setNome(result.getString("nome"));
+                cadastro.setTelefone(result.getLong("telefone"));
+                cadastro.setId(result.getInt("id"));
+                cadastros.add(cadastro);
+            }
+
+            for (Cadastro cadastro: cadastros){
+                System.out.println(cadastro.getId() + " | " + cadastro.getNome() + " | " +cadastro.getTelefone() );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return cadastros;
+    }
+
+    public void delete(int id){
+        try{
+            String sql = "DELETE FROM public.tb_mjv WHERE id=?;";
+            PreparedStatement statement =  connection.prepareStatement(sql);
+            statement.setInt(1, id);
+            statement.execute();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
